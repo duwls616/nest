6e3695b5bde3de2c043146e90221bb206b61d7af
matil <matilda94616@gmail.com>
2021-03-27 21:43:45 +0900

    add pipes

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,15 +1,16 @@
 import { 
     Body, Controller, Delete, Get, Param, Patch, Post, Put, Query 
 } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
 
-        constructor(private readonly moviesService: MoviesService) {
+    constructor(private readonly moviesService: MoviesService) {
 
-        }
+    }
 
     @Get()
     getAll() :Movie[]{
@@ -22,26 +23,23 @@ export class MoviesController {
     }
 
     @Get('/:id')
-    getOne(@Param("id") movieId:string) :Movie{ 
+    getOne(@Param("id") movieId:number) :Movie{ 
         return this.moviesService.getOne(movieId);
     }
 
     @Post()
-    create(@Body() movieData){
+    create(@Body() movieData: CreateMovieDto){
         return this.moviesService.create(movieData);
     }
 
     @Delete('/:id')
-    remove(@Param('id') movieId:string) {
+    remove(@Param('id') movieId:number) {
         return this.moviesService.deleteOne(movieId);
     }
 
     @Patch('/:id')
-    path(@Param('id') movieId: string, @Body() updateData) {
-        return {
-            updatedMovie: movieId,
-            ...updateData,
-        };
+    path(@Param('id') movieId: number, @Body() updateData) {
+        return this.moviesService.update(movieId, updateData);
     }
 
     
